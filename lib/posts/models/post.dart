4d3cdb3897406152ee6c0651b3ea1ad67675c2c5
Bruno Mazzocchi <bemazzocchi@gmail.com>
2023-02-12 21:42:43 -0600

    BLoC done :D

---
+++ lib/posts/models/post.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+// We extend Equatable so that we can compare Posts
+class Post extends Equatable {
+  const Post({required this.id, required this.title, required this.body});
+
+  final int id;
+  final String title;
+  final String body;
+
+  @override
+  List<Object> get props => [id, title, body];
+}
