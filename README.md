bfa61cbc1cecb68a18efa2905ff53bb2df6b9803
Bruno Mazzocchi <bemazzocchi@gmail.com>
2023-02-12 21:09:29 -0600

    Initial commit :D

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter Infinite List
+Flutter bloc tutorial, infinite list to fetch data continuously and load it as needed.
---
+++ android/app/src/main/kotlin/com/mazzocchi/flutter_infinite_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mazzocchi.flutter_infinite_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
